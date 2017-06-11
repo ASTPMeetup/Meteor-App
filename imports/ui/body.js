4a876943e65629079312ff432672c900d18db00a
Piero A. Pretto <piero.devops@gmail.com>
2017-06-11 15:30:24 -0500

    Create body.js

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -54,22 +54,18 @@ Template.body.events({
     // Insert a task into the collection if task is longer than 4 characters.
     if(text.length < 5) {
 		alert('task names must be at least 5 characters long.');
- 	}
-<<<<<<< HEAD
- 	else if(!date.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/g)) {
-=======
- 	else if(!date.match(/[0-9]{2}-[0-9]{2}-[0-9]{4}/g)) {
->>>>>>> bca955be336ad04fd605432b8dcdcb2c719c51c9
- 		alert('invalid date.');
- 	}
- 	else {
- 		Meteor.call('tasks.insert', text, date, priority, dueDate);
- 		// Clear form
-    	target.task.value = '';
-    	target.date.value = '';
-    	target.priority.value = '1';
-    	target.status.value = '1';
- 	}
+	}
+	else if(!date.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/g)) {
+		alert('invalid date.');
+	}
+	else {
+		Meteor.call('tasks.insert', text, date, priority, dueDate);
+		// Clear form
+		target.task.value = '';
+		target.date.value = '';
+		target.priority.value = '1';
+		target.status.value = '1';
+	}
   },
   'change .hide-completed input'(event, instance) {
       instance.state.set('hideCompleted', event.target.checked);
